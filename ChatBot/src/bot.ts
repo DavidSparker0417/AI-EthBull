10a5608a52b06217868901cbeb6b1b5c58be167e
DavidSparker <davidsparker0417@gmain.com>
2024-05-15 05:20:16 +0900

    initial project

---
+++ ChatBot/src/bot.ts
@@ -0,0 +1,159 @@
+import { Bot, InlineKeyboard } from "grammy";
+import { Variant, applyTextEffect, type Variant as TextEffectVaiant } from './textEffects'
+import { chunk } from "lodash";
+
+// Create a bot using the Telegram token
+const bot = new Bot(process.env.TELEGRAM_TOKEN || "")
+
+const introductionMessage = `Hello! I'm a Telegram bot.
+I'm powered by David, the next-generation serverless computing platform.
+
+<b>Commands</b>
+/yo - Be greeted by me
+/effect [text] - Show a keyboard to apply text effects to [text]`;
+
+const aboutRulKeyboard = new InlineKeyboard().url(
+  "Host you own bot for free.",
+  "https://cyclic.sh"
+)
+
+const replyWithIntro = (ctx: any) =>
+  ctx.reply(introductionMessage, {
+    reply_markup: aboutRulKeyboard,
+    parse_mode: "HTML",
+  });
+
+type Effect = { code: TextEffectVaiant; label: string };
+const allEffects: Effect[] = [
+  {
+    code: "w",
+    label: "Monospace",
+  },
+  {
+    code: "b",
+    label: "Bold",
+  },
+  {
+    code: "i",
+    label: "Italic",
+  },
+  {
+    code: "d",
+    label: "Doublestruck",
+  },
+  {
+    code: "o",
+    label: "Circled",
+  },
+  {
+    code: "q",
+    label: "Squared",
+  },
+];
+
+const effectCallbackCodeAccessor = (effectCode: TextEffectVaiant) => `effect-${effectCode}`
+
+const effectsKeyboardAccessor = (effectCodes: string[]) => {
+  const effectsAccessor = (effectCodes: string[]) =>
+    effectCodes.map((code) =>
+      allEffects.find((effect) => effect.code === code)
+    );
+  const effects = effectsAccessor(effectCodes)
+
+  const keyboard = new InlineKeyboard();
+  const chunkedEffects = chunk(effects, 3);
+  for (const effectsChunk of chunkedEffects) {
+    for (const effect of effectsChunk) {
+      effect && keyboard.text(effect.label, effectCallbackCodeAccessor(effect.code));
+    }
+    keyboard.row();
+  }
+  return keyboard
+}
+
+const textEffectResponseAccessor = (
+  originalText: string,
+  modifiedText?: string
+) =>
+  `Original: ${originalText}` +
+  (modifiedText ? `\nModified: ${modifiedText}` : "");
+
+const parseTextEffectResponse = (
+  response: string
+): {
+  originalText: string;
+  modifiedText?: string;
+} => {
+  const originalText = (response.match(/Original: (.*)/) as any)[1];
+  const modifiedTextMatch = response.match(/Modified: (.*)/);
+
+  let modifiedText;
+  if (modifiedTextMatch) modifiedText = modifiedTextMatch[1]
+
+  if (!modifiedTextMatch) return { originalText };
+  else return { originalText, modifiedText };
+};
+
+// Handle the /effect command to apply text effects using an inline keyboard
+bot.command('effect', (ctx) =>
+  ctx.reply(textEffectResponseAccessor(ctx.match), {
+    reply_markup: effectsKeyboardAccessor(
+      allEffects.map((effect) => effect.code)
+    ),
+  })
+);
+
+// Handle text effects from the effect keyboard
+for (const effect of allEffects) {
+  const allEffectCodes = allEffects.map((effect) => effect.code);
+  bot.callbackQuery(effectCallbackCodeAccessor(effect.code), async (ctx) => {
+    const { originalText } = parseTextEffectResponse(ctx.msg?.text || '');
+    const modifiedText = applyTextEffect(originalText, effect.code);
+    await ctx.editMessageText(
+      textEffectResponseAccessor(originalText, modifiedText),
+      {
+        reply_markup: effectsKeyboardAccessor(
+          allEffectCodes.filter((code) => code !== effect.code)
+        ),
+      }
+    );
+  });
+}
+
+const queryRegEx = /effect (monospace|bold|italic) (.*)/;
+bot.inlineQuery(queryRegEx, async (ctx) => {
+  const fullQuery = ctx.inlineQuery.query;
+  const fullQueryMatch = fullQuery.match(queryRegEx);
+  if (!fullQueryMatch) return;
+
+  const effectLabel = fullQueryMatch[1];
+  const originalText = fullQueryMatch[2];
+
+  const effectCode = allEffects.find(
+    (effect) => effect.label.toLowerCase() === effectLabel.toLowerCase()
+  )?.code;
+  const modifiedText = applyTextEffect(originalText, effectCode as Variant);
+
+  await ctx.answerInlineQuery(
+    [
+      {
+        type: "article",
+        id: 'text-effect',
+        title: 'Text Effects',
+        input_message_content: {
+          message_text: `Original: ${originalText} Modified: ${modifiedText}`,
+          parse_mode: "HTML",
+        },
+        reply_markup: new InlineKeyboard().switchInline("Share", fullQuery),
+        url: "http://t.me/david_1st_bot",
+        description: "Create stylish Unicode text, all within Telegram.",
+      },
+    ],
+    { cache_time: 30 * 24 * 3600 } // 30 month in seconds
+  );
+});
+
+bot.command("start", replyWithIntro);
+bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`))
+bot.on("message", replyWithIntro);
+bot.start()
